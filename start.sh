f4d521c59b42ecc79d79d151cd20b9e3ac081fff
Sven Speckmaier <sps@ipunkt.biz>
2019-02-15 13:46:27 +0100

    ignore filebeat logs,allow multiple target hosts

--- start.sh
+++ start.sh
@@ -2,7 +2,30 @@
 
 set -x
 
-sed -e "s/##HOSTURL##/${HOSTURL:-localhost:9200}/; s/##ENVIRONMENT##/${ENVIRONMENT:-Rancher}/; s/##PROTOCOL##/${PROTOCOL:-http}/; s/##USERNAME##/${USERNAME:-USER}/; s/##PASSWORD##/${PASSWORD:-PASSW}/" /var/filebeat.yml > /usr/share/filebeat/filebeat.yml
+URL="${HOSTURL:-http://localhost:9200}"
+ENVIRONMENT=${ENVIRONMENT:-Rancher}
+USERNAME="${USERNAME:-USER}"
+PASSWORD="${PASSWORD:-PASSW}"
+
+if [ ! -z "${URL}" ] ; then
+	OLD_IFS="${IFS}"
+	IFS=","
+
+	HOSTURL=""
+	for HOST in ${URL} ; do
+		HOSTURL="${HOSTURL}\"${HOST}\","
+	done
+	HOSTURL="${HOSTURL:0:-1}"
+
+	IFS="${OLD_IFS}"
+fi
+
+sed \
+	-e "s~##HOSTURL##~${HOSTURL}~" \
+	-e "s~##ENVIRONMENT##~${ENVIRONMENT}~" \
+	-e "s~##USERNAME##~${USERNAME}~" \
+	-e "s~##PASSWORD##~${PASSWORD}~" \
+	/var/filebeat.yml > /usr/share/filebeat/filebeat.yml
 
 
 /usr/local/bin/docker-entrypoint $*
